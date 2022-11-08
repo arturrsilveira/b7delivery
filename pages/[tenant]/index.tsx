49811e23f6e78930d1f7951b8c278152be223077
Artur Rafael da Silveira <arturrsilveira85@gmail.com>
2022-11-07 21:07:54 -0300

    Criação da Estrutura da página Home

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -2,8 +2,21 @@ import styles from "../../styles/Home.module.css";
 
 const Home = () => {
   return(
-    <div>
-      
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem-Vindo(a)! 👋</div>
+            <div className={styles.headerSubTitle}>O que deseja para hoje?</div>
+          </div>
+          <div className={styles.headerTopRight}>
+            
+          </div>
+        </div>
+        <div className={styles.headerBottom}>
+          Busca
+        </div>
+      </header>
     </div>
   )
 }
